732c8b4873dc028a14ea33e854aae73bcbecc2dc
majd443367 <ym.majzoob@gmail.com>
2026-04-28 21:51:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,32 +4,33 @@ function createBot() {
   const bot = mineflayer.createBot({
     host: 'Majzoob.aternos.me',
     port: 44336,
-    username: 'Bot123'
+    username: 'Bot_' + Math.floor(Math.random() * 1000),
+    version: '1.21.1'
   })
 
-  bot.on('spawn', () => {
-    console.log('Bot joined!')
+  bot.once('spawn', () => {
+    console.log('دخل السيرفر!')
 
-    // ينط كل 10 ثواني
+    // ينط كل 30 ثانية
     setInterval(() => {
       bot.setControlState('jump', true)
       setTimeout(() => bot.setControlState('jump', false), 500)
-    }, 10000)
+    }, 30000)
   })
 
-  bot.on('chat', (username, message) => {
-    if (message === 'hi') {
-      bot.chat('hello!')
-    }
-  })
-
-  // إذا طلع يرجع بعد 5 ثواني
+  // إذا طلع يرجع يدخل
   bot.on('end', () => {
-    console.log('Disconnected! Reconnecting in 5s...')
+    console.log('انفصل، عم يرجع يدخل...')
     setTimeout(createBot, 5000)
   })
 
-  bot.on('error', err => console.log(err))
+  bot.on('kicked', (reason) => {
+    console.log('انطرد:', reason)
+  })
+
+  bot.on('error', (err) => {
+    console.log('خطأ:', err)
+  })
 }
 
 createBot()
